d5dd4d4177ad2676ede9b49c5f8b29ccc30d76e3
Andres Joel Carrillo Rodriguez <andresjoe24@gmail.com>
2019-11-01 17:47:50 -0500

    Creación de componentes iniciales de la tienda

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div>
+        <nav class="site-navigation text-right text-md-center" role="navigation">
+        <div class="container">
+          <ul class="site-menu js-clone-nav d-none d-md-block">
+            <!-- <li class="has-children active"> -->
+            <li class="active">
+              <a href="index.html">Home</a>
+              <!-- <ul class="dropdown">
+                <li><a href="#">Menu One</a></li>
+                <li><a href="#">Menu Two</a></li>
+                <li><a href="#">Menu Three</a></li>
+                <li class="has-children">
+                  <a href="#">Sub Menu</a>
+                  <ul class="dropdown">
+                    <li><a href="#">Menu One</a></li>
+                    <li><a href="#">Menu Two</a></li>
+                    <li><a href="#">Menu Three</a></li>
+                  </ul>
+                </li>
+              </ul> -->
+            </li>
+            <!-- <li class="has-children">
+              <a href="about.html">About</a>
+              <ul class="dropdown">
+                <li><a href="#">Menu One</a></li>
+                <li><a href="#">Menu Two</a></li>
+                <li><a href="#">Menu Three</a></li>
+              </ul>
+            </li> -->
+            <li><a href="shop.html">Hombre</a></li>
+            <li><a href="#">Mujer</a></li>
+            <li><a href="#">Niños</a></li>
+            <li><a href="contact.html">Contacto</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+</template>
